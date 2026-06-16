52d87718c247478e4d57cbb5eb8e788efa6ee4bf
Vishal Shaji <vishshaji03@gmail.com>
2026-06-16 06:12:45 +0530

    Add: house-robber - submission-5

---
+++ Data Structures & Algorithms/house-robber/submission-5.py
@@ -0,0 +1,12 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        # SPACE EFFICIENT METHOD
+        
+        if len(nums) == 1:      # Edge case
+            return nums[0]
+        rob1, rob2 = nums[0], max(nums[0],nums[1])
+        #dp[1] would be the biggest of the first 2 naturally.
+        for i in range(2,len(nums)):
+            rob1, rob2 = max(rob2, rob1+nums[i]), rob1
+            #two choices, either rob or rob next. no point skipping more than 1 since all number positive
+        return rob1
